463fe702d9f51b4afee5c34538e9dfc3adf8e1ed
Filipe Sales <66375370+filipesalesaraujo@users.noreply.github.com>
2021-10-18 21:39:55 -0300

    config db dev

--- wp-config.php
+++ wp-config.php
@@ -1,70 +1,40 @@
 <?php
-
 $environments = array(
-	'development' => '',
-	'production' => '',
+	'development' => 'dev.filipesalesaraujo.online',
+	'production' => 'filipesalesaraujo.online',
 );
 $server_name = $_SERVER['SERVER_NAME'];
-
 foreach ($environments as $key => $env) {
 	if (strstr($server_name, $env)) {
 		define('ENVIRONMENT', $key);
 		break;
 	}
 }
-
 if (!defined('ENVIRONMENT')) define('ENVIRONMENT', 'production');
 switch (ENVIRONMENT) {
-
+	case '':
+		define('DB_NAME', 'u997469977_bd_dev');
+		define('DB_USER', 'u997469977_user_bd_dev');
+		define('DB_PASSWORD', ';1Hw#wF;weAk');
+		define('DB_HOST', 'localhost');
+		define('DB_CHARSET', 'utf8');
+		define('DB_COLLATE', '');
+		define('WP_DEBUG', true);
+		define('WP_CACHE', false);
+		break;
 	case '':
 		define('DB_NAME', 'DBNAME');
 		define('DB_USER', 'DBUSER');
 		define('DB_PASSWORD', 'PASSWORD');
 		define('DB_HOST', 'localhost');
-		define('WP_SITEURL', '');
-		define('WP_HOME', '');
-		define('WP_DEBUG', true);
-		define('WP_CACHE', false);
-		break;
-
-	case '':
-		define('DB_NAME', 'DBNAME');
-		define('DB_USER', 'DBUSER');
-		define('DB_PASSWORD', 'PASSWORD');
-		define('DB_HOST', '');
-		define('DB_HOST_SLAVE', '');
 		define('DB_CHARSET', 'utf8');
-		define('DB_COLLATE', 'utf8_unicode_ci');
-		define('WP_SITEURL', '');
-		define('WP_HOME', '/');
+		define('DB_COLLATE', '');
 		define('WP_DEBUG', false);
-		define('WP_CACHE', TRUE);
+		define('WP_CACHE', true);
 		break;
 }
-
-if (!defined('DB_NAME')) define('DB_NAME', 'DBNAME');
-if (!defined('DB_USER')) define('DB_USER', 'DBUSER');
-if (!defined('DB_PASSWORD')) define('DB_PASSWORD', 'PASSWORD');
-if (!defined('DB_HOST')) define('DB_HOST', '');
-if (!defined('DB_HOST_SLAVE')) define('DB_HOST_SLAVE', '');
-if (!defined('DB_CHARSET')) define('DB_CHARSET', 'utf8');
-if (!defined('DB_COLLATE')) define('DB_COLLATE', 'utf8_unicode_ci');
-if (!defined('WP_SITEURL')) define('WP_SITEURL', '');
-if (!defined('WP_HOME')) define('WP_HOME', '');
-if (!defined('WP_DEBUG')) define('WP_DEBUG', false);
-if (!defined('WP_CACHE')) define('WP_CACHE', TRUE);
-
-$table_prefix = 'wp_';
-
-define('AUTH_KEY',         'coloque a sua frase única aqui');
-define('SECURE_AUTH_KEY',  'coloque a sua frase única aqui');
-define('LOGGED_IN_KEY',    'coloque a sua frase única aqui');
-define('NONCE_KEY',        'coloque a sua frase única aqui');
-define('AUTH_SALT',        'coloque a sua frase única aqui');
-define('SECURE_AUTH_SALT', 'coloque a sua frase única aqui');
-define('LOGGED_IN_SALT',   'coloque a sua frase única aqui');
-define('NONCE_SALT',       'coloque a sua frase única aqui');
-
+$table_prefix = 'wu997469977p_';
+define( 'WP_MEMORY_LIMIT',  '256M' );
 if (!defined('ABSPATH'))
 	define('ABSPATH', dirname(__FILE__) . '/');
 require_once(ABSPATH . 'wp-settings.php');
