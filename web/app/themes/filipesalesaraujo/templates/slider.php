d4c92940fcd4746edd72498e3d146f2b7a689815
Filipe Sales <66375370+filipesalesaraujo@users.noreply.github.com>
2021-12-27 18:13:29 -0300

    Update slider.php

--- web/app/themes/filipesalesaraujo/templates/slider.php
+++ web/app/themes/filipesalesaraujo/templates/slider.php
@@ -7,8 +7,8 @@
       <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/img/banners/banner-2.jpg" alt="Banner 2" class="t-swiper__img" data-scroll>
     </div>
   </div>
-  <div class="t-swiper__pagination js-swiper__pagination swiper-pagination" data-scroll></div>
+  <!-- <div class="t-swiper__pagination js-swiper__pagination swiper-pagination" data-scroll></div>
   <div class="t-swiper__button-prev js-swiper__button-prev swiper-button-prev" data-scroll></div>
   <div class="t-swiper__button-next js-swiper__button-next swiper-button-next" data-scroll></div>
-  <div class="t-swiper__scrollbar js-swiper__scrollbar swiper-scrollbar" data-scroll></div>
+  <div class="t-swiper__scrollbar js-swiper__scrollbar swiper-scrollbar" data-scroll></div> -->
 </div>
