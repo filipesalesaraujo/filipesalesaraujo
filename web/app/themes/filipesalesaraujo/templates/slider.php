f571d196ec3c7b7ce1eda5ff80722c79a866f665
Filipe Sales <66375370+filipesalesaraujo@users.noreply.github.com>
2021-12-27 17:16:07 -0300

    Update slider.php

--- web/app/themes/filipesalesaraujo/templates/slider.php
+++ web/app/themes/filipesalesaraujo/templates/slider.php
@@ -1,14 +1,14 @@
 <section class="t-swiper js-swiper" data-scroll-section>
-  <div class="t-swiper__wrapper js-swiper__wrapper">
-    <div class="t-swiper__slide js-swiper__slide">
+  <div class="t-swiper__wrapper js-swiper__wrapper" data-scroll>
+    <div class="t-swiper__slide js-swiper__slide" data-scroll>
       <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/img/banners/banner-1.png" alt="Banner 1" class="t-swiper__img" data-scroll>
     </div>
-    <div class="t-swiper__slide js-swiper__slide">
+    <div class="t-swiper__slide js-swiper__slide" data-scroll>
       <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/img/banners/banner-2.jpg" alt="Banner 2" class="t-swiper__img" data-scroll>
     </div>
   </div>
-  <div class="t-swiper__pagination js-swiper__pagination"></div>
-  <div class="t-swiper__button-prev js-swiper__button-prev"></div>
-  <div class="t-swiper__button-next js-swiper__button-next"></div>
-  <div class="t-swiper__scrollbar js-swiper__scrollbar"></div>
+  <div class="t-swiper__pagination js-swiper__pagination" data-scroll></div>
+  <div class="t-swiper__button-prev js-swiper__button-prev" data-scroll></div>
+  <div class="t-swiper__button-next js-swiper__button-next" data-scroll></div>
+  <div class="t-swiper__scrollbar js-swiper__scrollbar" data-scroll></div>
 </section>
