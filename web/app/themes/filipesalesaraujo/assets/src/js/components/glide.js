caf08d33eace7bc5f92609edd0058cc8d41e8adf
Filipe Sales <66375370+filipesalesaraujo@users.noreply.github.com>
2021-12-27 05:56:19 -0300

    carousel -> slider

--- web/app/themes/filipesalesaraujo/assets/src/js/components/glide.js
+++ web/app/themes/filipesalesaraujo/assets/src/js/components/glide.js
@@ -1,8 +1,7 @@
 import Glide from '@glidejs/glide';
 export default function () {
-    new Glide('.js-carousel', {
-        type: 'carousel',
-        startAt: 0,
+    new Glide('.js-slider', {
+        type: 'slider',
         perView: 1,
         autoplay: 3000,
         hoverpause: true,
