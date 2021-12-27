0a19e35b2287fef864bced27f76c6af69a423306
Filipe Sales <66375370+filipesalesaraujo@users.noreply.github.com>
2021-12-27 17:40:38 -0300

    swiper teste

--- web/app/themes/filipesalesaraujo/assets/src/js/components/swiper.js
+++ web/app/themes/filipesalesaraujo/assets/src/js/components/swiper.js
@@ -5,8 +5,11 @@ export default function () {
     const swiper = new Swiper('.js-swiper', {
         direction: 'vertical',
         loop: true,
+        slidesPerView: 1,
+        centeredSlides: true,
         pagination: {
             el: '.js-swiper__pagination',
+            clickable: true,
         },
         navigation: {
             nextEl: '.js-swiper__button-next',
