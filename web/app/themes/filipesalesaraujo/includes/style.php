2cd387059fc03ff5a48c2e82ebd0e21e51ee02df
Filipe Sales <66375370+filipesalesaraujo@users.noreply.github.com>
2021-12-27 19:36:40 -0300

    swiper - css

--- web/app/themes/filipesalesaraujo/includes/style.php
+++ web/app/themes/filipesalesaraujo/includes/style.php
@@ -1,7 +1,7 @@
 <?php
 function theme_enqueue_style()
 {
-  $versionStyle = '1.8';
+  $versionStyle = '1.9';
   $versionBundleStyle = '1.1';
   wp_enqueue_style('style', get_template_directory_uri() . '/assets/dist/css/style.min.css', array(), $versionStyle, 'all');
   wp_enqueue_style('bundle-style', get_template_directory_uri() . '/assets/dist/css/bundle.min.css', array(), $versionBundleStyle, 'all');
