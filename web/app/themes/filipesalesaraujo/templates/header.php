4b2ccdda8dadf89c4a0b26a636a5240d9847d397
Filipe Sales <66375370+filipesalesaraujo@users.noreply.github.com>
2021-12-27 04:41:19 -0300

    Update header.php

--- web/app/themes/filipesalesaraujo/templates/header.php
+++ web/app/themes/filipesalesaraujo/templates/header.php
@@ -2,13 +2,13 @@
     <div class="t-header__grid" data-scroll>
         <nav class="t-header__nav">
             <li class="t-header__li">
-                <a href="<?php bloginfo('') ?>" class="t-header__link">Home</a>
+                <a href="<?php bloginfo('url') ?>" class="t-header__link">Home</a>
             </li>
             <li class="t-header__li">
-                <a href="<?php bloginfo('') ?>/about/" class="t-header__link">About</a>
+                <a href="<?php bloginfo('url') ?>/about/" class="t-header__link">About</a>
             </li>
             <li class="t-header__li">
-                <a href="<?php bloginfo('') ?>/contact/" class="t-header__link">Contact</a>
+                <a href="<?php bloginfo('url') ?>/contact/" class="t-header__link">Contact</a>
             </li>
         </nav>
     </div>
