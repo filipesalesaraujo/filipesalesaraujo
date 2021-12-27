69931e70036d25bdd74334dfe3aa6e2ecbc9bbcf
Filipe Sales <66375370+filipesalesaraujo@users.noreply.github.com>
2021-12-27 17:24:00 -0300

    Update slider.php

--- web/app/themes/filipesalesaraujo/templates/slider.php
+++ web/app/themes/filipesalesaraujo/templates/slider.php
@@ -1,9 +1,9 @@
 <section class="t-swiper js-swiper" data-scroll-section>
-  <div class="t-swiper__wrapper js-swiper__wrapper" data-scroll>
-    <div class="t-swiper__slide js-swiper__slide" data-scroll>
+  <div class="t-swiper__wrapper" data-scroll>
+    <div class="t-swiper__slide" data-scroll>
       <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/img/banners/banner-1.png" alt="Banner 1" class="t-swiper__img" data-scroll>
     </div>
-    <div class="t-swiper__slide js-swiper__slide" data-scroll>
+    <div class="t-swiper__slide" data-scroll>
       <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/img/banners/banner-2.jpg" alt="Banner 2" class="t-swiper__img" data-scroll>
     </div>
   </div>
