5b5d0ff9e1ba5292b5b4c1f001cbd069504513c6
Filipe Sales <66375370+filipesalesaraujo@users.noreply.github.com>
2021-12-27 18:05:54 -0300

    swiper test

--- web/app/themes/filipesalesaraujo/assets/src/js/components/swiper.js
+++ web/app/themes/filipesalesaraujo/assets/src/js/components/swiper.js
@@ -1,21 +1,21 @@
-//   import Swiper from 'https://unpkg.com/swiper@7/swiper-bundle.esm.browser.min.js'
-// import 'swiper/css/bundle';
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
 export default function () {
     const swiper = new Swiper('.js-swiper', {
         direction: 'vertical',
         loop: true,
         slidesPerView: 1,
         centeredSlides: true,
-        pagination: {
-            el: '.js-swiper__pagination',
-            clickable: true,
-        },
-        navigation: {
-            nextEl: '.js-swiper__button-next',
-            prevEl: '.js-swiper__button-prev',
-        },
-        scrollbar: {
-            el: '.js-swiper__scrollbar',
-        },
+        // pagination: {
+        //     el: '.js-swiper__pagination',
+        //     clickable: true,
+        // },
+        // navigation: {
+        //     nextEl: '.js-swiper__button-next',
+        //     prevEl: '.js-swiper__button-prev',
+        // },
+        // scrollbar: {
+        //     el: '.js-swiper__scrollbar',
+        // },
     });
 }
--- web/app/themes/filipesalesaraujo/templates/footer.php
+++ web/app/themes/filipesalesaraujo/templates/footer.php
@@ -1,4 +1,2 @@
 <footer class="footer" data-scroll-section>
 </footer>
-<link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
-<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
