2f80c5cecf5db560df5f4f11bbca0c354471f873
Filipe Sales <66375370+filipesalesaraujo@users.noreply.github.com>
2021-12-26 23:56:28 -0300

    Update functions.php

--- web/app/themes/filipesalesaraujo/functions.php
+++ web/app/themes/filipesalesaraujo/functions.php
@@ -5,3 +5,8 @@ require_once($template_diretorio . "/includes/logo.php");
 require_once($template_diretorio . "/includes/menu.php");
 require_once($template_diretorio . "/includes/style.php");
 require_once($template_diretorio . "/includes/thumbnail.php");
+
+function remove_menus(){  
+  remove_menu_page( 'plugins.php' );                //Plugins  
+}  
+add_action( 'admin_menu', 'remove_menus' );  
