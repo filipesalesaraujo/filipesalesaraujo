ba3bdb35fa64e77945f313513b49d5ead5f0971c
Filipe Sales <66375370+filipesalesaraujo@users.noreply.github.com>
2021-12-27 03:33:49 -0300

    functions: remove logo and menu, add admin-menu

--- web/app/themes/filipesalesaraujo/functions.php
+++ web/app/themes/filipesalesaraujo/functions.php
@@ -1,17 +1,6 @@
 <?php
 $template_diretorio = get_template_directory();
 require_once($template_diretorio . "/includes/bundle.php");
-require_once($template_diretorio . "/includes/logo.php");
-require_once($template_diretorio . "/includes/menu.php");
 require_once($template_diretorio . "/includes/style.php");
 require_once($template_diretorio . "/includes/thumbnail.php");
-
-function remove_menus()
-{
-    remove_menu_page('plugins.php');                //Plugins /wp/wp-admin/plugins.php
-    remove_menu_page('edit-comments.php');          //Comments /wp/wp-admin/edit-comments.php
-    remove_menu_page('options-general.php');        //Settings /wp/wp-admin/options-general.php
-    remove_menu_page('tools.php');                  //Tools /wp/wp-admin/tools.php
-    remove_menu_page('users.php');                  //Users /wp/wp-admin/users.php
-}
-add_action('admin_menu', 'remove_menus');
+require_once($template_diretorio . "/includes/admin-menu.php");
--- web/app/themes/filipesalesaraujo/includes/menu.php
+++
@@ -1,6 +0,0 @@
-<?php
-function theme_menu()
-{
-    register_nav_menu('menu', __('Menu'));
-}
-add_action('init', 'theme_menu');
