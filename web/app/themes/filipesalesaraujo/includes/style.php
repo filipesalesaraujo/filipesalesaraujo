52cb8b58445a78df4c3d1e00701fdd5338ce021d
Filipe Sales <66375370+filipesalesaraujo@users.noreply.github.com>
2021-12-27 04:20:21 -0300

    @fontsource/chakra-petch

--- web/app/themes/filipesalesaraujo/includes/style.php
+++ web/app/themes/filipesalesaraujo/includes/style.php
@@ -1,7 +1,7 @@
 <?php
 function theme_enqueue_style()
 {
-  $versionStyle = '1.3';
+  $versionStyle = '1.4';
   wp_enqueue_style('style', get_template_directory_uri() . '/assets/dist/css/style.min.css', array(), $versionStyle, 'all');
 }
 add_action('wp_enqueue_scripts', 'theme_enqueue_style');
