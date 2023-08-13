f160693e2132d5704767da1c1336c8e5dc8857b8
filipesalesaraujo <filipesalesaraujo@gmail.com>
2023-08-12 23:19:13 -0300

    :duck:

---
+++ src/app/sitemap.ts
@@ -0,0 +1,30 @@
+import {MetadataRoute} from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    return [
+        {
+            url: 'https://filipesalesaraujo.com/',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://filipesalesaraujo.com/artigos',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://filipesalesaraujo.com/artigos/aplicacao-aprendizado-maquina-reducao-desperdicio-alimentos-acacia-saude',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://filipesalesaraujo.com/artigos/implementacao-aprendizado-nao-supervisionado-segmentacao-clientes-banco-beta',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://filipesalesaraujo.com/artigos/implementacao-banco-dados-nosql-industria-automobilistica',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://filipesalesaraujo.com/artigos/web-semantica-ferramenta-gestao-pacientes-diabeticos',
+            lastModified: new Date(),
+        },
+    ]
+}
