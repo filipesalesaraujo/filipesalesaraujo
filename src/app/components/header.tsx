076d6834cc0e91a3e4776e9feec3529fc479d5e0
filipesalesaraujo <filipesalesaraujo@gmail.com>
2023-08-27 01:39:59 -0300

    :duck:

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -6,9 +6,9 @@ export default function Header() {
             <div className="max-w-[1140px] w-[100%] flex justify-between gap-5 p-5">
                 <nav>
                     <ul className="flex gap-5">
-                        <li><Link href='/' className="text-gray-500 hover:text-gray-300 transition-colors">Início</Link></li>
-                        <li><Link href='/artigos' className="text-gray-500 hover:text-gray-300 transition-colors">Artigos</Link></li>
-                        <li><Link href='/contato' className="text-gray-500 hover:text-gray-300 transition-colors">Contato</Link></li>
+                        <li><Link href='/' className="text-gray-300 hover:text-white transition-colors">Início</Link></li>
+                        <li><Link href='/artigos' className="text-gray-300 hover:text-white transition-colors">Artigos</Link></li>
+                        <li><Link href='/contato' className="text-gray-300 hover:text-white transition-colors">Contato</Link></li>
                     </ul>
                 </nav>
             </div>
