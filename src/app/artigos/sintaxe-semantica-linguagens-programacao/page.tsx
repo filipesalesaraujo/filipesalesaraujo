cfe774c59ef0fbdda0362afbbc0606e396beb714
filipesalesaraujo <filipesalesaraujo@gmail.com>
2023-08-27 17:09:01 -0300

    :duck:

--- src/app/artigos/sintaxe-semantica-linguagens-programacao/page.tsx
+++ src/app/artigos/sintaxe-semantica-linguagens-programacao/page.tsx
@@ -64,18 +64,15 @@ export default function SintaxeSemanticaProg() {
             <main>
                 <section className="flex justify-center items-center">
                     <div className="max-w-[1140px] w-[100%] p-5 gap-5 flex flex-col">
-                        <Image src={featuredImage} alt="A web semêntica como ferramenta facilitadora na gestão de pacientes diabéticos" className="rounded-xl" />
+                        <Image src={featuredImage} alt="A web semêntica como ferramenta facilitadora na gestão de pacientes diabéticos" className="rounded-xl"/>
                         <h1 className="text-3xl font-extrabold bg-gradient-linear bg-clip-text text-transparent">Sintaxe e Semântica em Linguagens de Programação: Entendendo a Estrutura e o Significado no Código</h1>
-                        <hr className="text-gray-300"/>
                         <p className="text-gray-300">Dentro do vasto mundo da programação, os conceitos de sintaxe e semântica desempenham papéis fundamentais na estruturação e compreensão de códigos. Ambos, embora distintos, são essenciais para o desenvolvimento de programas eficientes e corretos. Neste texto, exploraremos suas diferenças e importâncias, ilustradas através de exemplos práticos (MCCONNELL, 2004).</p>
-                        <hr className="text-gray-300"/>
-                        <h2 className="text-gray-300 text-2xl">Sintaxe</h2>
+                        <h2 className="text-3xl font-extrabold bg-gradient-linear bg-clip-text text-transparent">Sintaxe</h2>
                         <p className="text-gray-300">A sintaxe, em sua essência, pode ser comparada à gramática em linguagens humanas. É o conjunto de regras que define a estrutura correta das sentenças ou instruções em uma linguagem de programação (LUDWIG, 2015). Um código pode ser escrito seguindo rigorosamente essas regras, garantindo que o compilador ou interpretador o compreenda sem problemas.</p>
                         <p className="text-gray-300">Exemplo de erro de sintaxe em Python:</p>
                         <code className="text-gray-300 bg-black p-1 rounded-sm">for i in range(5) <br/>print(i)</code>
                         <p className="text-gray-300">No exemplo acima, a falta de dois pontos após range(5) resulta em um erro de sintaxe, impedindo o programa de ser executado.</p>
-                        <hr className="text-gray-300"/>
-                        <h3 className="text-gray-300 text-2xl">Semântica</h3>
+                        <h3 className="text-3xl font-extrabold bg-gradient-linear bg-clip-text text-transparent">Semântica</h3>
                         <p className="text-gray-300">A semântica aborda a intenção por trás do código: o que realmente se espera que ele faça. Mesmo que um código seja sintaticamente correto, se não realizar a ação esperada, ele sofre de erros semânticos (MARTIN, 2019). Estes são, muitas vezes, mais desafiadores de identificar, pois emergem apenas durante a execução ou ao observar os resultados do programa.</p>
                         <p className="text-gray-300">Exemplo de erro semântico em Python:</p>
                         <code className="text-gray-300 bg-black p-1 rounded-sm">
@@ -86,9 +83,9 @@ export default function SintaxeSemanticaProg() {
                             print(area) # Deveria imprimir 25, mas imprime 10.
                         </code>
                         <p className="text-gray-300">No trecho de código acima, embora a função area_quadrado esteja correta sintaticamente, ela tem um erro semântico, pois, ao invés de calcular a área do quadrado multiplicando o lado por si mesmo, ela simplesmente adiciona o lado a si mesmo.</p>
-                        <hr className="text-gray-300"/>
+                        <h4 className="text-3xl font-extrabold bg-gradient-linear bg-clip-text text-transparent">Entrelaçando Sintaxe e Semântica no Desenvolvimento de Software</h4>
                         <p className="text-gray-300">Sintaxe e semântica são pilares complementares em programação. Enquanto a sintaxe garante que o código seja interpretável pela máquina, a semântica assegura que ele execute as intenções desejadas. Para criar programas eficazes, ambos os aspectos devem ser meticulosamente considerados e revistos.</p>
-                        <hr className="text-gray-300"/>
+                        <h5 className="text-3xl font-extrabold bg-gradient-linear bg-clip-text text-transparent">Referências Bibliográficas</h5>
                         <p className="text-gray-300">MCCONNELL, Steve. <strong>Código Completo.</strong> 2ª ed. Microsoft Press, 2004.</p>
                         <p className="text-gray-300">LUDWIG, Larry. <strong>Programação:</strong> Princípios e Prática Usando C++. 2ª ed. Pearson Education, 2015. </p>
                         <p className="text-gray-300">MARTIN, Robert C. <strong>Código Limpo.</strong> Alta Books, 2019.</p>
