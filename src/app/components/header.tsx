49e455604eb5f4eb65b3b7d303d6b6707cf5e1e2
filipesalesaraujo <filipesalesaraujo@gmail.com>
2023-09-13 18:52:32 -0300

    :duck:

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -4,6 +4,7 @@ export default function Header() {
     return (
         <header className="bg-black flex justify-center items-center border-b-[1px] bottom-2 border-gray-500">
             <div className="max-w-[1140px] w-[100%] flex justify-between gap-5 p-5">
+               <p className="text-gray-300 font-bold">[filipesalesaraujo]</p>
                 <nav>
                     <ul className="flex gap-5">
                         <li><Link href='/' className="text-gray-300 hover:text-white transition-colors">Início</Link></li>
