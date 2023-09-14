ed17cb0a35d5afe4eda3716ad2ee8a84fae17526
filipesalesaraujo <filipesalesaraujo@gmail.com>
2023-09-13 21:07:50 -0300

    :duck:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,6 +58,15 @@ const jsonLd: WithContext<Person> = {
 
 import dominandoNextjsImage from '../../public/images/dominando-nextjs.webp'
 import Link from "next/link";
+import importanciaDoWebpDesempenhoPagesPagespeedInsights from "../../public/images/importancia-do-webp-desempenho-pages-pagespeed-insights.webp";
+import concorrenciaDistribuidaESeusDesafiosEmSistemasMultiprocessadores from "../../public/images/concorrencia-distribuida-e-seus-desafios-em-sistemas-multiprocessadores.webp";
+import sintaxeSemanticaLinguagensProgramacao from "../../public/images/sintaxe-semantica-linguagens-programacao.webp";
+import estrategiasImplementacaoModelosCreditoBancoBhcRMachineLearning from "../../public/images/estrategias-implementacao-modelos-credito-banco-bhc-r-machine-learning.webp";
+import otimizacaoResolucaoProblema8RainhasInteligenciaArtificial from "../../public/images/otimizacao-resolucao-problema-8-rainhas-inteligencia-artificial.webp";
+import aplicacaoAprendizadoMaquinaReducaoDesperdicioAlimentosAcaciaSaude from "../../public/images/aplicacao-aprendizado-maquina-reducao-desperdicio-alimentos-acacia-saude.webp";
+import implementacaoBancoDadosNosqlIndustriaAutomobilistica from "../../public/images/implementacao-banco-dados-nosql-industria-automobilistica.webp";
+import implementacaoAprendizadoNaoSupervisionadoSegmentacaoClientesBancoBeta from "../../public/images/implementacao-aprendizado-nao-supervisionado-segmentacao-clientes-banco-beta.webp";
+import webSemanticaFerramentaGestaoPacientesDiabeticos from "../../public/images/web-semantica-ferramenta-gestao-pacientes-diabeticos.webp";
 
 export default function Home() {
     return (
@@ -84,6 +93,42 @@ export default function Home() {
                             <a href="https://wa.me/5511984583529?text=Olá!%20Gostaria%20de%20entrar%20em%20contato%20com%20você." target="_blank" className="bg-green-500 bg-opacity-50 hover:bg-opacity-80 text-gray-300 flex items-center justify-center p-1 rounded-md font-bold transition-colors">Quer melhorar a performance do seu site? Clique aqui e saiba mais!</a>
                         </div>
 
+                        <div className="flex justify-between">
+                            <h1 className="text-3xl font-extrabold bg-gradient-linear bg-clip-text text-transparent">Artigos recentes</h1>
+                            <Link className="text-gray-300 hover:text-gray-500 transition-colors" href='/artigos/'>Ver todos</Link>
+                        </div>
+                        <div className="flex flex-row flex-wrap gap-5">
+
+                            <div className="border-[1px] border-gray-500 rounded-md p-3 xl:max-w-[32.1%] w-[100%] flex flex-col justify-between gap-5">
+                                <div className="flex flex-col gap-5">
+                                    <Image src={importanciaDoWebpDesempenhoPagesPagespeedInsights} alt="Concorrência Distribuída e Seus Desafios em Sistemas Multiprocessadores" className="rounded-md"/>
+                                    <Link href='/artigos/importancia-do-webp-desempenho-pages-pagespeed-insights' className="text-xl font-bold text-gray-300 hover:text-white transition-colors">A Importância do WEBP para o Desempenho das Páginas no PageSpeed Insights</Link>
+                                    <p className="text-gray-300">WEBP acelera sites com compressão superior, superando JPEG e PNG. É a escolha ideal para velocidade na web moderna.</p>
+                                </div>
+
+                                <Link href='/artigos/importancia-do-webp-desempenho-pages-pagespeed-insights' className="bg-zinc-800 text-zinc-400 flex items-center justify-center p-1 rounded-md font-bold hover:text-zinc-300 hover:bg-zinc-700 transition-colors">Leia mais</Link>
+                            </div>
+
+                            <div className="border-[1px] border-gray-500 rounded-md p-3 xl:max-w-[32.1%] w-[100%] flex flex-col justify-between gap-5">
+                                <div className="flex flex-col gap-5">
+                                    <Image src={concorrenciaDistribuidaESeusDesafiosEmSistemasMultiprocessadores} alt="Concorrência Distribuída e Seus Desafios em Sistemas Multiprocessadores" className="rounded-md"/>
+                                    <Link href='/artigos/concorrencia-distribuida-e-seus-desafios-em-sistemas-multiprocessadores' className="text-xl font-bold text-gray-300 hover:text-white transition-colors">Concorrência Distribuída e Seus Desafios em Sistemas Multiprocessadores</Link>
+                                    <p className="text-gray-300">Sistemas multiprocessadores enfrentam desafios como race conditions e DeadLocks. Compreender esses problemas é crucial para a eficiência.</p>
+                                </div>
+
+                                <Link href='/artigos/concorrencia-distribuida-e-seus-desafios-em-sistemas-multiprocessadores' className="bg-zinc-800 text-zinc-400 flex items-center justify-center p-1 rounded-md font-bold hover:text-zinc-300 hover:bg-zinc-700 transition-colors">Leia mais</Link>
+                            </div>
+
+                            <div className="border-[1px] border-gray-500 rounded-md p-3 xl:max-w-[32.1%] w-[100%] flex flex-col justify-between gap-5">
+                                <div className="flex flex-col gap-5">
+                                    <Image src={sintaxeSemanticaLinguagensProgramacao} alt="Concorrência Distribuída e Seus Desafios em Sistemas Multiprocessadores" className="rounded-md"/>
+                                    <Link href='/artigos/sintaxe-semantica-linguagens-programacao' className="text-xl font-bold text-gray-300 hover:text-white transition-colors">Sintaxe e Semântica em Linguagens de Programação: Entendendo a Estrutura e o Significado no Código</Link>
+                                    <p className="text-gray-300">Sintaxe e semântica são fundamentais na programação. Erros sintáticos e semânticos impactam execução e comportamento do código.</p>
+                                </div>
+                                <Link href='/artigos/sintaxe-semantica-linguagens-programacao' className="bg-zinc-800 text-zinc-400 flex items-center justify-center p-1 rounded-md font-bold hover:text-zinc-300 hover:bg-zinc-700 transition-colors">Leia mais</Link>
+                            </div>
+
+                        </div>
 
                     </div>
                 </section>
