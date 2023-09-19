d1140eaffea70ea925de8790129ecdb2d787edde
filipesalesaraujo <filipesalesaraujo@gmail.com>
2023-09-18 22:15:30 -0300

    :duck:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,10 @@ const crimsonPro = Crimson_Pro({subsets: ['latin']})
 import {GetStaticProps, GetStaticPropsContext} from "next";
 import Link from "next/link";
 import Image from "next/image";
+import {TbBrandGoogleAnalytics, TbDeviceDesktopAnalytics} from "react-icons/tb";
+import {SiPagespeedinsights} from "react-icons/si";
+import {MdScreenSearchDesktop} from "react-icons/md";
+import {FaLaptopCode} from "react-icons/fa";
 
 export interface TPost {
     _embedded: any;
@@ -52,12 +56,42 @@ export default function Home({posts}: IBlog) {
                 <p className="text-3xl">No cenário digital atual, é essencial equilibrar inovação com confiabilidade. Por isso, orgulho-me de trabalhar com tecnologia de ponta, como o <strong className="underline font-normal decoration-2 underline-offset-2">Next.js</strong>, para proporcionar experiências web rápidas e dinâmicas. Ao mesmo tempo, valorizo a robustez e versatilidade de plataformas consolidadas, como o <strong className="underline font-normal decoration-2 underline-offset-2">WordPress</strong>. Esta combinação permite-me entregar soluções modernas, eficientes e confiáveis para qualquer desafio que venha pela frente.</p>
             </section>
 
+            <div className="bg-black w-[100%] flex justify-center items-center">
+                <div className="max-w-4xl p-5 flex gap-5 flex-col w-[100%] text-white">
+                    <div className="w-[100%] max-w-[100%] flex flex-col gap-5">
+                        <h2 className="text-3xl ">Soluções</h2>
+
+                       <div className="flex gap-5 justify-between flex-col lg:flex-row">
+                           <div className="flex flex-col rounded-md border-white border-[1px] p-5 gap-5 items-center lg:w-1/4 justify-between">
+                               <div className="text-3xl"><TbDeviceDesktopAnalytics/></div>
+                               <p className="text-center">Google Analytics 4</p>
+                           </div>
+
+                           <div className="flex flex-col rounded-md border-white border-[1px] p-5 gap-5 items-center lg:w-1/4 justify-between">
+                               <div className="text-3xl"><SiPagespeedinsights/></div>
+                               <p className="text-center">PageSpeed Insights</p>
+                           </div>
+
+                           <div className="flex flex-col rounded-md border-white border-[1px] p-5 gap-5 items-center lg:w-1/4 justify-between">
+                               <div className="text-3xl"><MdScreenSearchDesktop/></div>
+                               <p className="text-center">Google Search Console</p>
+                           </div>
+                           <div className="flex flex-col rounded-md border-white border-[1px] p-5 gap-5 items-center lg:w-1/4 justify-between">
+                               <div className="text-3xl"><FaLaptopCode/></div>
+                               <p className="text-center">Desenvolvimento Web</p>
+                           </div>
+                       </div>
+
+                    </div>
+                </div>
+            </div>
+
             <div className="max-w-4xl p-5 flex gap-5 flex-col">
 
-               <div className="w-[100%] flex flex-col lg:flex-row justify-between gap-2 items-start">
-                   <h2 className="text-3xl ">Artigos em destaque</h2>
-                   <Link href="/artigos" className="underline-animation">ver todos</Link>
-               </div>
+                <div className="w-[100%] flex flex-col lg:flex-row justify-between gap-2 items-start">
+                    <h3 className="text-3xl ">Artigos em destaque</h3>
+                    <Link href="/artigos" className="underline-animation">ver todos</Link>
+                </div>
 
                 <div className="flex gap-5 flex-col lg:flex-row">
                     {posts.map((post) => (
