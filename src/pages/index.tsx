4622ef722b9169cf5bed453f2f861749438826a3
filipesalesaraujo <filipesalesaraujo@gmail.com>
2023-09-24 22:47:45 -0300

    :duck:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import {Crimson_Pro} from 'next/font/google'
 
 const crimsonPro = Crimson_Pro({subsets: ['latin']})
 
+import i from '../../public/i.png'
 
 import {GetStaticProps, GetStaticPropsContext} from "next";
 import Link from "next/link";
@@ -52,8 +53,15 @@ export default function Home({posts}: IBlog) {
                     <span className={crimsonPro.className}>Oi! Meu nome é </span>
                     <strong className="text-7xl block">Filipe Sales Araujo</strong>
                 </h1>
-                <p className="text-lg">Com formação em <strong className="underline font-normal decoration-2 underline-offset-2">Educação Física</strong> e experiência em docência, tanto em ambientes escolares quanto em academias. Mergulhei no universo do <strong className="underline font-normal decoration-2 underline-offset-2">desenvolvimento web</strong> e estou aprofundando meus conhecimentos em <strong className="underline font-normal decoration-2 underline-offset-2">Ciência de Dados</strong> através de uma pós-graduação. Minha trajetória multifacetada me permite conectar diferentes mundos e trazer uma perspectiva única aos projetos. </p>
-                <p className="text-lg">No cenário digital atual, é essencial equilibrar inovação com confiabilidade. Por isso, orgulho-me de trabalhar com tecnologia de ponta, como o <strong className="underline font-normal decoration-2 underline-offset-2">Next.js</strong>, para proporcionar experiências web rápidas e dinâmicas. Ao mesmo tempo, valorizo a robustez e versatilidade de plataformas consolidadas, como o <strong className="underline font-normal decoration-2 underline-offset-2">WordPress</strong>. Esta combinação permite-me entregar soluções modernas, eficientes e confiáveis para qualquer desafio que venha pela frente.</p>
+
+                <div className="flex gap-5 items-start flex-col md:flex-row">
+                    <div className="flex md:w-1/2 flex-col gap-5">
+                        <p className="text-lg">Com formação em <strong className="underline font-normal decoration-2 underline-offset-2">Educação Física</strong> e experiência em docência, tanto em ambientes escolares quanto em academias. Mergulhei no universo do <strong className="underline font-normal decoration-2 underline-offset-2">desenvolvimento web</strong> e estou aprofundando meus conhecimentos em <strong className="underline font-normal decoration-2 underline-offset-2">Ciência de Dados</strong> através de uma pós-graduação. Minha trajetória multifacetada me permite conectar diferentes mundos e trazer uma perspectiva única aos projetos. </p>
+                        <p className="text-lg">No cenário digital atual, é essencial equilibrar inovação com confiabilidade. Por isso, orgulho-me de trabalhar com tecnologia de ponta, como o <strong className="underline font-normal decoration-2 underline-offset-2">Next.js</strong>, para proporcionar experiências web rápidas e dinâmicas. Ao mesmo tempo, valorizo a robustez e versatilidade de plataformas consolidadas, como o <strong className="underline font-normal decoration-2 underline-offset-2">WordPress</strong>. Esta combinação permite-me entregar soluções modernas, eficientes e confiáveis para qualquer desafio que venha pela frente.</p>
+                    </div>
+                    <Image className="flex md:w-1/2" src={i} alt="Quadrinhos de uma pessoa de óculos"/>
+                </div>
+
             </section>
 
             <div className="bg-black w-[100%] flex justify-center items-center">
@@ -61,26 +69,26 @@ export default function Home({posts}: IBlog) {
                     <div className="w-[100%] max-w-[100%] flex flex-col gap-5">
                         <h2 className="text-3xl ">Soluções</h2>
 
-                       <div className="flex gap-5 justify-between flex-col lg:flex-row">
-                           <div className="flex flex-col rounded-md border-white border-[1px] p-5 gap-5 items-center lg:w-1/4 justify-between">
-                               <div className="text-3xl"><TbDeviceDesktopAnalytics/></div>
-                               <p className="text-lg text-center">Google Analytics 4</p>
-                           </div>
+                        <div className="flex gap-5 justify-between flex-col lg:flex-row">
+                            <div className="flex flex-col rounded-md border-white border-[1px] p-5 gap-5 items-center lg:w-1/4 justify-between">
+                                <div className="text-3xl"><TbDeviceDesktopAnalytics/></div>
+                                <p className="text-lg text-center">Google Analytics 4</p>
+                            </div>
 
-                           <div className="flex flex-col rounded-md border-white border-[1px] p-5 gap-5 items-center lg:w-1/4 justify-between">
-                               <div className="text-3xl"><SiPagespeedinsights/></div>
-                               <p className="text-lg text-center">PageSpeed Insights</p>
-                           </div>
+                            <div className="flex flex-col rounded-md border-white border-[1px] p-5 gap-5 items-center lg:w-1/4 justify-between">
+                                <div className="text-3xl"><SiPagespeedinsights/></div>
+                                <p className="text-lg text-center">PageSpeed Insights</p>
+                            </div>
 
-                           <div className="flex flex-col rounded-md border-white border-[1px] p-5 gap-5 items-center lg:w-1/4 justify-between">
-                               <div className="text-3xl"><MdScreenSearchDesktop/></div>
-                               <p className="text-lg text-center">Google Search Console</p>
-                           </div>
-                           <div className="flex flex-col rounded-md border-white border-[1px] p-5 gap-5 items-center lg:w-1/4 justify-between">
-                               <div className="text-3xl"><FaLaptopCode/></div>
-                               <p className="text-lg text-center">Desenvolvimento Web</p>
-                           </div>
-                       </div>
+                            <div className="flex flex-col rounded-md border-white border-[1px] p-5 gap-5 items-center lg:w-1/4 justify-between">
+                                <div className="text-3xl"><MdScreenSearchDesktop/></div>
+                                <p className="text-lg text-center">Google Search Console</p>
+                            </div>
+                            <div className="flex flex-col rounded-md border-white border-[1px] p-5 gap-5 items-center lg:w-1/4 justify-between">
+                                <div className="text-3xl"><FaLaptopCode/></div>
+                                <p className="text-lg text-center">Desenvolvimento Web</p>
+                            </div>
+                        </div>
 
                     </div>
                 </div>
@@ -96,7 +104,7 @@ export default function Home({posts}: IBlog) {
                 <div className="flex gap-5 flex-col lg:flex-row">
                     {posts.map((post) => (
                             <div key={post.id} className="border-[1px] border-black w-[100%] flex flex-col justify-between rounded-md overflow-hidden">
-                                <Image width={1680} height={720} src={post._embedded['wp:featuredmedia']['0'].source_url}  alt={post.title.rendered}/>
+                                <Image width={1680} height={720} src={post._embedded['wp:featuredmedia']['0'].source_url} alt={post.title.rendered}/>
                                 <div className="gap-5 p-5 flex flex-col justify-between h-[100%] items-start rounded-md overflow-hidden">
                                     <div className="flex flex-col gap-5 ">
                                         <h1 className="font-bold text-lg	">{post.title.rendered}</h1>
