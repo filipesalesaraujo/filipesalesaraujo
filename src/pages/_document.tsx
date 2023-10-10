1cc425c19a239372e34d468188108681f5fcb790
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2023-10-10 15:09:24 -0300

    :duck:

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -51,18 +51,15 @@ export default function Document() {
             <header className="flex items-center justify-center bg-[#27282c]">
                 <div className="max-w-7xl w-[100%] flex justify-between items-center">
                     <div className="p-5 lg:min-w-[243px] lg:max-w-[19%] w-[100%] justify-start bg-black">
-                        <Link href="/"
-                              className="text-white hover:text-white text-xs lg:text-2xl font-bold bg-black hover:opacity-80 transition-opacity">
+                        <Link href="/" className="text-white hover:text-white text-xs lg:text-2xl font-bold bg-black hover:opacity-80 transition-opacity">
                             [filipesalesaraujo]
                         </Link>
                     </div>
                     <div className="p-5 lg:max-w-[81%] w-[100%] flex justify-end items-center gap-5">
-                        <Link href="/"
-                              className="text-gray-400 hover:text-white transition-colors text-xs lg:text-2xl">Início</Link>
-                        <Link href='/blog'
-                              className="text-gray-400 hover:text-white transition-colors text-xs lg:text-2xl">Blog</Link>
+                        <Link href="/" className="text-gray-400 hover:text-white transition-colors text-xs lg:text-2xl">Início</Link>
+                        <Link href="/sobre" className="text-gray-400 hover:text-white transition-colors text-xs lg:text-2xl">Sobre</Link>
+                        <Link href='/blog' className="text-gray-400 hover:text-white transition-colors text-xs lg:text-2xl">Blog</Link>
                         <a href="mailto:filipesalesaraujo@gmail.com" target="_blank" className="text-gray-400 hover:text-white transition-colors text-xs lg:text-2xl">Contato</a>
-
                     </div>
                 </div>
             </header>
---
+++ src/pages/sobre.tsx
@@ -0,0 +1,35 @@
+export default function Sobre() {
+    return (
+        <main className="flex items-center justify-center flex-col">
+            <section className="max-w-7xl px-5 py-10 flex flex-wrap gap-10 flex-col bg-home bg-no-repeat bg-[center_-10px]">
+                <h1 className="text-4xl text-center">
+                    <span>Oi! Meu nome é </span>
+                    <strong className="text-7xl block">Filipe Sales Araujo</strong>
+                </h1>
+                <div className="flex gap-5 items-start flex-col md:flex-row">
+                    <div className="flex flex-col gap-5 items-start">
+                        <p className="text-2xl">Formado em Licenciatura em Educação Física e Bacharel em <strong>Ciência
+                            da Computação</strong>, minha trajetória acadêmica se enriquece a cada dia. Atualmente,
+                            estou me aprofundando ainda mais no universo da tecnologia e estou realizando uma
+                            pós-graduação em<strong> Ciência de Dados</strong>. Esta formação combinada permite que eu
+                            possa abordar desafios com uma perspectiva única, unindo a disciplina do corpo e mente com a
+                            precisão e inovação da computação.</p>
+                        <p className="text-2xl">No desenvolvimento de sites, priorizo sempre a eficiência e a
+                            otimização. É essencial para mim que a velocidade de carregamento das páginas esteja à
+                            altura das melhores práticas sugeridas pelo <strong>PageSpeed Insights</strong>. Além disso,
+                            tenho especial
+                            atenção à correta indexação das páginas, usando ferramentas como o Google Search Console,
+                            garantindo que o conteúdo seja facilmente encontrado e classificado. Não menos importante,
+                            utilizo o GA4 para analisar o tráfego dos sites, compreendendo assim o comportamento dos
+                            usuários e otimizando a experiência online.</p>
+                        <p className="text-2xl">No âmbito técnico, escolhi usar o <strong>Next.js</strong> no front-end
+                            devido à sua eficiência e adaptabilidade. Para o back-end, optei pelo <strong>WordPress
+                                Headless</strong>, uma solução robusta e flexível que se integra perfeitamente ao
+                            conjunto, proporcionando um desempenho superior e uma experiência de usuário mais fluida.
+                        </p>
+                    </div>
+                </div>
+            </section>
+        </main>
+    )
+}
