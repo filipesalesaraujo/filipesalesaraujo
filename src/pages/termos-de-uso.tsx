a847fb08cc79b315ca4e80ca1113093ab17046a8
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2023-10-14 17:21:36 -0300

    :duck:

--- src/pages/termos-de-uso.tsx
+++ src/pages/termos-de-uso.tsx
@@ -1,15 +1,29 @@
 export default function TermosDeUso() {
     return (
-        <main className="flex items-center justify-center flex-col">
+        <main className="flex items-center justify-center flex-col bg-about bg-cover bg-no-repeat bg-[center_-30px]">
             <section className="max-w-7xl px-5 py-10 flex flex-wrap gap-10 flex-col ">
+
                 <h1 className="text-4xl font-bold">Termos de Uso</h1>
-                <p className="text-lg">Ao acessar e usar nosso site, você aceita e concorda em estar vinculado a estes Termos de Uso e a todas as leis e regulamentos aplicáveis. Estes termos representam um acordo legal entre você e filipesalesaraujo.com e governam o seu acesso e utilização deste site.</p>
-                <p className="text-lg">Todo o conteúdo disponibilizado em filipesalesaraujo.com, seja ele em forma de textos, gráficos, imagens, logos, ícones ou vídeos, é propriedade exclusiva de filipesalesaraujo.com e está protegido por leis internacionais de direitos autorais. Qualquer tentativa de reprodução, distribuição ou transmissão de partes ou totalidade do conteúdo, sem a autorização prévia e por escrito de filipesalesaraujo.com, é estritamente proibida.</p>
-                <p className="text-lg">O conteúdo presente em filipesalesaraujo.com é fornecido apenas para sua informação pessoal e não comercial. É proibido o uso deste conteúdo para fins comerciais, incluindo reprodução, distribuição, modificação ou exibição, sem o devido consentimento. Aqueles que optarem por acessar e usar o conteúdo de maneira não autorizada estarão sujeitos a sanções legais.</p>
-                <p className="text-lg">Ainda que envidemos todos os esforços para garantir a precisão e atualidade das informações, filipesalesaraujo.com não oferece garantias quanto à exatidão, relevância ou completude do conteúdo publicado. Não nos responsabilizamos por eventuais erros, omissões ou imprecisões que possam ocorrer.</p>
-                <p className="text-lg">Sob nenhuma circunstância filipesalesaraujo.com ou suas partes afiliadas serão responsáveis por danos resultantes do uso ou da incapacidade de usar o site ou seu conteúdo, seja esses danos diretos, indiretos, incidentais ou consequentes.</p>
-                <p className="text-lg">Reforçamos que reservamos o direito de modificar ou substituir estes Termos de Uso a qualquer momento e a nosso critério. Alterações nos termos terão efeito imediato após sua publicação no site, e o uso contínuo do site após tais alterações implica na aceitação destas.</p>
-                <p className="text-lg">Caso tenha dúvidas ou preocupações relacionadas a estes termos, por favor, entre em contato conosco através do filipesalesaraujo@gmail.com.</p>
+                <p className="text-lg">Ao acessar e utilizar nosso site, você concorda em cumprir e estar vinculado a estes Termos de Uso, bem como a todas as leis e regulamentos aplicáveis. Estes termos constituem um contrato legalmente vinculante entre você, o usuário, e filipesalesaraujo.com e regulam o acesso e a utilização deste site.</p>
+
+                <h2 className="text-4xl font-bold">Propriedade de Conteúdo</h2>
+                <p className="text-lg">Todo o conteúdo disponibilizado em filipesalesaraujo.com, incluindo, mas não se limitando a textos, gráficos, imagens, logos, ícones ou vídeos, é considerado propriedade exclusiva de filipesalesaraujo.com e está protegido por leis internacionais de direitos autorais. Qualquer tentativa de reprodução, distribuição ou transmissão, total ou parcial, do conteúdo, sem a autorização prévia e por escrito de filipesalesaraujo.com, constitui uma violação dos direitos autorais e é estritamente proibida.</p>
+
+                <h3 className="text-4xl font-bold">Uso do Conteúdo</h3>
+                <p className="text-lg">O conteúdo disponível em filipesalesaraujo.com é fornecido unicamente para fins de informação pessoal e não comercial. O uso deste conteúdo para fins comerciais, incluindo reprodução, distribuição, modificação ou exibição pública, sem a devida autorização, é estritamente proibido. Qualquer usuário que acesse e utilize o conteúdo sem autorização estará sujeito a ações legais em conformidade com as leis de direitos autorais vigentes.</p>
+
+                <h4 className="text-4xl font-bold">Precisão das Informações</h4>
+                <p className="text-lg">Embora filipesalesaraujo.com se esforce para manter a precisão e a atualidade das informações apresentadas, não oferecemos garantias expressas ou implícitas quanto à exatidão, relevância ou completude do conteúdo. Não nos responsabilizamos por erros, omissões ou imprecisões nas informações apresentadas no site. Os usuários são aconselhados a verificar a precisão das informações antes de tomar decisões ou ações com base nelas.</p>
+
+                <h5 className="text-4xl font-bold">Responsabilidade e Danos</h5>
+                <p className="text-lg">Em nenhuma circunstância, filipesalesaraujo.com ou suas partes afiliadas serão responsáveis por danos diretos, indiretos, incidentais, consequentes ou de qualquer outra natureza decorrentes do uso ou da incapacidade de usar o site ou seu conteúdo. O uso do site é de responsabilidade exclusiva do usuário, que assume todos os riscos associados a ele. filipesalesaraujo.com isenta-se de qualquer responsabilidade por perdas ou danos resultantes do uso ou confiança nas informações disponibilizadas no site.</p>
+
+                <h6 className="text-4xl font-bold">Alterações nos Termos de Uso</h6>
+                <p className="text-lg">Reservamos o direito de modificar ou substituir estes Termos de Uso a qualquer momento e a nosso critério. Quaisquer alterações nos termos entrarão em vigor imediatamente após sua publicação no site, e o uso contínuo do site após tais modificações será considerado como aceitação das mesmas. Os usuários são encorajados a revisar regularmente os Termos de Uso para acompanhar quaisquer atualizações ou modificações.</p>
+
+                <p className="text-4xl font-bold">Contato</p>
+                <p className="text-lg">Para dúvidas ou preocupações relacionadas a estes termos, entre em contato conosco através do e-mail filipesalesaraujo@gmail.com. Estamos disponíveis para fornecer informações adicionais ou esclarecimentos técnicos, conforme necessário. Seu feedback é fundamental para melhorar nossos serviços e a qualidade das informações que fornecemos.</p>
+
             </section>
         </main>
     )
